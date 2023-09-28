498ddcb1259e8cf24f4c1c58082c8199c2c5de11
Heiker Curiel <heikercuriel@gmail.com>
2023-09-28 10:47:44 -0400

    chore: update plugins

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,17 +2,19 @@ local Plugin = {'lukas-reineke/indent-blankline.nvim'}
 
 Plugin.name = 'indent_blankline'
 
+Plugin.main = 'ibl'
+
 Plugin.event = {'BufReadPost', 'BufNewFile'}
 
-Plugin.main = "ibl"
-
--- See :help indent-blankline-setup
+-- See :help ibl.setup()
 Plugin.opts = {
-  -- char = '▏',
-  -- show_trailing_blankline_indent = false,
-  -- show_first_indent_level = false,
-  -- use_treesitter = true,
-  -- show_current_context = false
+  enabled = true,
+  scope = {
+    enabled = true,
+  },
+  indent = {
+    char = '▏',
+  },
 }
 
 return Plugin
