f3b49e90a9ec0d4220093f0440b1edc205c4935c
Ethan Warrick <ewboy12@gmail.com>
2023-09-28 10:03:26 -0500

    Command: Add ctags support
    This ports over ctags from Jim's config.

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -16,3 +16,24 @@ vim.api.nvim_create_autocmd('FileType', {
   command = 'nnoremap <buffer> q <cmd>quit<cr>'
 })
 
+-- TODO: this should be coverted to lua
+-- This ports over ctags functionality
+vim.api.nvim_exec([[
+    " Ctags
+    function! s:CtagsHandler(job_id, data, event)
+        if a:event == 'exit'
+            echo "Generating Tags... Complete"
+        endif
+    endfunction
+    function! GenerateCtags()
+        let l:ctags_cmd = "ctags -R --fields=+Slk --c-types=+deftuxgsmp -B --exclude=build --exclude=megainclude ."
+        echo "Generating Tags..."
+        if has('nvim')
+            let job = jobstart(l:ctags_cmd, {'on_exit': function('s:CtagsHandler')})
+        else
+            call system(l:ctags_cmd)
+            call s:CtagsHandler(0, "", 'exit')
+        endif
+    endfunction
+    nnoremap <F12> :call GenerateCtags()<CR> 
+]], false)
