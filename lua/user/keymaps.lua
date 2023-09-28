0b73884b4af0a71eeb2698fe692d7f71f03aa6c0
Ethan Warrick <ewboy12@gmail.com>
2023-09-28 10:04:18 -0500

    Keymap: copy path
    Add keymaps to copy the current file path, both relative and absolute.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -20,3 +20,8 @@ vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>') -- Buffer quit
 vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>') -- Buffer last
 vim.keymap.set('n', '<leader>bn', '<cmd>bnext<cr>') -- Buffer next
 vim.keymap.set('n', '<leader>bp', '<cmd>bprevious<cr>') -- Buffer previous
+
+-- Copy path
+vim.keymap.set('n', '<leader>p', ':let @" = expand("%")<cr>') -- Copy relative path
+vim.keymap.set('n', '<leader>P', ':let @" = expand("%:p")<cr>') -- Copy absolute path
+
