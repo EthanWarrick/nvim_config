b3fe312002f9dc0e9efc5201afb265a6e1e8b34c
Ethan Warrick <ewboy12@gmail.com>
2023-09-28 10:04:25 -0500

    Plugin: web-devicons: Add
    Adding this plugin shows file type icons in nvim tree and bufferline.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ local Plugins = {
   {'tpope/vim-fugitive'},
   {'wellle/targets.vim'},
   {'tpope/vim-repeat'},
-  -- {'kyazdani42/nvim-web-devicons', lazy = true},
+  {'kyazdani42/nvim-web-devicons', lazy = true},
   {'numToStr/Comment.nvim', config = true, event = 'VeryLazy'},
 	{'christoomey/vim-tmux-navigator'},
 	{'folke/which-key.nvim'},
