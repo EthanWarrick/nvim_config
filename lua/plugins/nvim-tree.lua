f1a703da9a235d592c60ec7a1bde7a245b448580
Ethan Warrick <ewboy12@gmail.com>
2023-09-28 10:01:24 -0500

    Plugin: nvim-tree: Don't move cursor when open file
    this binds the enter key to open files from the nvim-tree buffer but
    prevents the cursor from switching windows. The idea behind this is to
    enable opening multiple files quickly from the nvim-tree buffer.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -18,6 +18,12 @@ Plugin.opts = {
     bufmap('L', api.node.open.edit, 'Expand folder or go to file')
     bufmap('H', api.node.navigate.parent_close, 'Close parent folder')
     bufmap('gh', api.tree.toggle_hidden_filter, 'Toggle hidden files')
+
+    -- This opens a file then moves the cursor back to the file explorer window
+    bufmap('<cr>', function()
+      api.node.open.edit() -- Call the nvim-tree edit api call (see nvim-tree docs)
+      vim.cmd("wincmd t") -- Call vimscript goto previous window
+    end, 'Expand folder or open file')
   end
 }
 
