12540671fd07d0f494e2fcc45aef660c39332140
Ethan Warrick <ewboy12@gmail.com>
2023-10-02 11:24:18 -0500

    Plugin: grep-op: Add
    Added this plugin from Tim. It allows you to grep search using vim
    operators.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,11 @@ local Plugins = {
   {'kyazdani42/nvim-web-devicons', lazy = true},
   {'numToStr/Comment.nvim', config = true, event = 'VeryLazy'},
 	{'christoomey/vim-tmux-navigator'},
+  {
+    "mertzt89/grep-op.nvim",
+    config = true,
+    lazy = false,
+  },
 }
 
 return Plugins
