abcac267141b1f26c70cc5a9e62a4736b8b1fc17
Ethan Warrick <ewboy12@gmail.com>
2023-10-02 10:09:03 -0500

    Plugin: indent-blankline: Update to v3
    Updating this plugin from v2 to v3. This is a big change in the plugin
    configuration. There is a big API to look through to make custom
    configs. Off the bat, the new version/config is highlighting the top
    line of the current scope now.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -4,13 +4,15 @@ Plugin.name = 'indent_blankline'
 
 Plugin.event = {'BufReadPost', 'BufNewFile'}
 
+Plugin.main = "ibl"
+
 -- See :help indent-blankline-setup
 Plugin.opts = {
-  char = '▏',
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = false,
-  use_treesitter = true,
-  show_current_context = false
+  -- char = '▏',
+  -- show_trailing_blankline_indent = false,
+  -- show_first_indent_level = false,
+  -- use_treesitter = true,
+  -- show_current_context = false
 }
 
 return Plugin
