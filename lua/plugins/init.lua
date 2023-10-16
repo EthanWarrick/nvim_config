f8596638440b58e14a503acf48f14ca764abcd30
Ethan Warrick <ewboy12@gmail.com>
2023-10-16 15:48:16 -0500

    Plugin: re-format

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,11 +5,7 @@ local Plugins = {
   {'kyazdani42/nvim-web-devicons', lazy = true},
   {'numToStr/Comment.nvim', config = true, event = 'VeryLazy'},
 	{'christoomey/vim-tmux-navigator'},
-  {
-    "mertzt89/grep-op.nvim",
-    config = true,
-    lazy = false,
-  },
+  {"mertzt89/grep-op.nvim", config = true, lazy = false},
 }
 
 return Plugins
