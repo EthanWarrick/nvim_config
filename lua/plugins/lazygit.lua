0782a99a7c42ce852bf0bd669c1c21a847cdf813
Ethan Warrick <ewboy12@gmail.com>
2023-11-06 12:05:11 -0600

    Plugin: lazygit: Add commits window keymap

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -3,7 +3,8 @@ local Plugin = {'kdheepak/lazygit.nvim'}
 Plugin.cmd = {'LazyGit', 'LazyGitConfig', 'LazyGitCurrentFile', 'LazyGitFilter', 'LazyGitFilterCurrentFile'}
 
 function Plugin.init()
-  vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>')
+  vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>') -- Open Lazygit window
+  vim.keymap.set('n', '<leader>G', '<cmd>LazyGitFilter<cr>') -- Open Lazygit commits window
 end
 
 return Plugin
