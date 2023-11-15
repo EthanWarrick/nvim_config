5f959e5a7d365901cf1ec896edad4aa7f7de37a0
Ethan Warrick <ewboy12@gmail.com>
2023-11-15 15:29:10 -0600

    Plugin: telescope: set flex layout
    Previously, a few keymaps were specified to use a vertical layout. These
    have been changed to use the default layout and the default layout has
    been set to flex. The flex layout dynamically chooses between a
    horizontal and vertical layout based on the width of the window.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,14 +13,15 @@ function Plugin.init()
   -- See :help telescope.builtin
   vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
   vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
-  vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files layout_strategy=vertical<cr>')
-  vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep layout_strategy=vertical<cr>')
+  vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+  vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
   vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
-  vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find layout_strategy=vertical<cr>')
+  vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
 end
 
 Plugin.opts = {
   defaults = {
+    layout_strategy = 'flex',
   }
 }
 
