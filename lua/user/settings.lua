aed8f5448389a7d25bf48f907b121b9a10841b0f
Ethan Warrick <ewboy12@gmail.com>
2023-12-04 18:17:55 -0600

    Setting: Format status column
    Setup desired behavior for line numbers and the sign column. See the
    comment in the commit for more info.

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,14 +1,43 @@
-vim.opt.number         = true   -- Show line numbers
+-------------------------------- Status Column --------------------------------
+-- This status line configuration consists of the sign column, the fold column,
+--  and the line number column. The size of the sign and fold columns are
+--  automatic. They are both only present if there is something to display and
+--  are at minimum two and one characters wide, respectively. The size of the
+--  line number column is wide enough to fit the max line number plus a space.
+--  These were formatted using the functionality of status column. The line
+--  number column shows the current line number for the current line and
+--  relative line numbers for others. The current line number is preceeded by
+--  zeros filling the whole line number column width. All line numbers are
+--  aligned to the right.
+
+vim.opt.statuscolumn =
+  '%s%C%=%{%v:relnum?"%r":"%0"..(float2nr(log10(line("$")))+1).."l"%} '
+
+-- Line Numbers
+vim.opt.number         = true                 -- Show line numbers
+vim.opt.relativenumber = true                 -- Show relative line numbers
+vim.opt.numberwidth    = 1                    -- Width for number column
+vim.opt.wrap           = true                 -- Wrap text
+
+-- Sign Column
+vim.opt.signcolumn     = 'auto'               -- Width for sign column
+
+-- Fold Column
+vim.opt.foldcolumn     = 'auto'               -- Width for fold column
+vim.opt.fillchars = {                         -- Fold column characters
+  foldopen = '',
+  foldclose = '',
+}
+-------------------------------------------------------------------------------
+
 vim.opt.mouse          = 'a'    -- Turn on mouse mode
 vim.opt.ignorecase     = true   -- Ignore capitalization when searching a file
 vim.opt.smartcase      = true   -- Stop ignoring capitalization when searching a capital letter
 vim.opt.hlsearch       = false  -- Highlight search term
-vim.opt.wrap           = true   -- Wrap text
 vim.opt.breakindent    = true   -- Preserve indentation when wrapping text
 vim.opt.tabstop        = 2      -- Tab size
 vim.opt.shiftwidth     = 2      -- Number of spaces for (auto)indent
 vim.opt.expandtab      = true   -- Turn tab into spaces
-vim.opt.signcolumn     = 'yes'
 vim.opt.cursorline     = true   -- Highlight the entire line containing the cursor
 vim.opt.colorcolumn    = '81'   -- Highlight a vertical column for visual character limit
 
