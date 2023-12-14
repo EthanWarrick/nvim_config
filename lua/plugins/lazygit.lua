e03f809b5e4b52c4d50839baf43e082c157ab55b
Ethan Warrick <ewboy12@gmail.com>
2023-12-14 14:48:35 -0600

    Plugin: lazygit: Use plenary window
    Using the plenary plugin to create the lazygit window makes the border a
    little nicer. This change sets the config options to use the plenary
    window creation function and sets the background transparency to zero.
    The lazygit.nvim plugin didn't respect the transparency config option
    when used with the plenary option. I forked the plugin repo and added
    the functionality, hence the change in the plugin source.

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,8 +1,15 @@
 local Plugin = {'kdheepak/lazygit.nvim'}
 
+Plugin.dependencies = {
+  {'nvim-lua/plenary.nvim'},
+}
+
 Plugin.cmd = {'LazyGit', 'LazyGitConfig', 'LazyGitCurrentFile', 'LazyGitFilter', 'LazyGitFilterCurrentFile'}
 
 function Plugin.init()
+  vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+  vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+
   vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>') -- Open Lazygit window
   vim.keymap.set('n', '<leader>G', '<cmd>LazyGitFilter<cr>') -- Open Lazygit commits window
 end
