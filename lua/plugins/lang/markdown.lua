a1d3734955be77190d42f0e444c9df38e7dc1ec3
Ethan Warrick <ewboy12@gmail.com>
2024-01-09 14:26:00 -0600

    Lang: markdown: Add LSP and grip
    This adds a language file for markdown. It installs the LSP as well as a
    plugin to add grip control functionality from vim. Grip is a python
    package for rendering markdown files as html. It locally hosts a
    web-server to view the rendered html in a browser and can export the
    html as well. Grip is convenient for editing markdown because it
    automatically refreshes the webpage after the corresponding markdown
    file has been saved.
    
    I tried to make the vim-grip plugin lazy load. This commit only
    implements lazy loading on opening a markdown file. I could not get lazy
    loading to work on the plugin's commands. I think this is because there
    are issues with how lazy.nvim handles pure vimscript plugins.
    
    I have added a keymap to open the current buffer in grip. This will only
    work if the current buffer is a markdown file. This should probably be
    improved to not run if it isn't a valid situation. Also, I probably
    would want toggle functionality instead of on/off functionality.

---
+++ lua/plugins/lang/markdown.lua
@@ -0,0 +1,30 @@
+return {
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+    optional = true,
+		opts = {
+			servers = {
+				-- Ensure mason installs the server
+				 marksman = {},
+			},
+		},
+	},
+
+  -- A plugin to activate the python package grip to view markdown
+  --  files in a browser.
+  {
+    "PratikBhusal/vim-grip",
+    enabled = os.execute("which grip") == 0, -- Only enable if grip is installed
+    ft = {'md', 'mkdn', 'mdown', 'markdown'}, -- Lazy load on these files
+    -- cmd = {'GripStart', 'GripExport'}, -- Lazy load on these commands
+    init = function()
+      vim.g.grip_default_map = 0 -- Don't use the default plugin keymappings
+
+      vim.keymap.set('n', '<leader>m', '<cmd>GripStart<cr>') -- Start grip and open in browser
+      vim.keymap.set('n', '<leader>M', '<cmd>GripStop<cr>') -- Kill grip
+    end,
+  },
+
+}
