76f87f1f7611123b7942aeee4efbb6189c3599ec
Ethan Warrick <ewboy12@gmail.com>
2024-01-09 18:43:16 -0600

    Plugin: lazygit: Change keymap initialization
    I was already effectively lazy-loading on a keymap anyway since the
    keymap was calling the lazy-loading command. I want to get rid of the
    Plugin.init() function because some of the plugin's global variables
    get assigned default values when the plugin loads. The init function
    always happens at neovim startup so some variables will be overwritten.
    The better place to assign these global variables would be in
    Plugin.config() that runs only when the plugin loads.

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -5,13 +5,15 @@ Plugin.dependencies = {
 }
 
 Plugin.cmd = {'LazyGit', 'LazyGitConfig', 'LazyGitCurrentFile', 'LazyGitFilter', 'LazyGitFilterCurrentFile'}
+Plugin.keys = {
+  {'<leader>g', '<cmd>LazyGit<cr>', mode = 'n', desc = 'Open Lazygit Window'},
+  {'<leader>G', '<cmd>LazyGitFilter<cr>', mode = 'n', desc = 'Open Lazygit Commits Window'},
+}
 
 function Plugin.init()
   vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
   vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
 
-  vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<cr>') -- Open Lazygit window
-  vim.keymap.set('n', '<leader>G', '<cmd>LazyGitFilter<cr>') -- Open Lazygit commits window
 end
 
 return Plugin
