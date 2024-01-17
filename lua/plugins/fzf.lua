20494948cc3a65082e23bdf1e90da55ecfd4c2a8
Ethan Warrick <ewboy12@gmail.com>
2024-01-17 10:25:51 -0600

    Plugin: fzf: Use generalized grep operator
    Also reformat and add some keybindings

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -8,36 +8,53 @@ Plugin.dependencies = {
 }
 
 Plugin.cmd = {'FzfLua',}
-Plugin.keys = {
-  {'<leader>fb', function() require('fzf-lua').buffers() end,              mode = 'n', desc = 'buffers'},
-  {'<leader>fr', function() require('fzf-lua').registers() end,            mode = 'n', desc = 'View registers'},
-  {'<leader>fm', function() require('fzf-lua').marks() end,                mode = 'n', desc = 'View marks'},
-  {'<leader>fc', function() require('fzf-lua').command_history() end,      mode = 'n', desc = 'View command history'},
-  {'<leader>ff', function() require('fzf-lua').files() end,                mode = 'n', desc = 'Find files'},
-  {'<leader>fg', function() require('fzf-lua').live_grep() end,            mode = 'n', desc = 'Live grep'},
-  {'<leader>fs', function() require('fzf-lua').lgrep_curbuf() end,         mode = 'n', desc = 'Current buffer fuzzy find'},
-  {'<leader>fd', function() require('fzf-lua').diagnostics_document() end, mode = 'n', desc = 'List document diagnostics'},
-  {'<leader>gc', function() require('fzf-lua').git_commits() end,          mode = 'n', desc = 'Current buffer fuzzy find'},
-  {'<leader>gC', function() require('fzf-lua').git_bcommits() end,         mode = 'n', desc = 'Current buffer fuzzy find'},
-  {'<leader>gb', function() require('fzf-lua').git_branches() end,         mode = 'n', desc = 'Current buffer fuzzy find'},
+Plugin.keys = function()
+  local fzf = require("fzf-lua")
+  return {
+    -- Buffers and Files --
+    {'<leader>fb', function() fzf.buffers() end,              mode = 'n', desc = 'Find buffers'},
+    {'<leader>ff', function() fzf.files() end,                mode = 'n', desc = 'Find files'},
+    -- Search --
+    {
+      "gs",
+      require("util").grep_operator(function(query)
+        fzf.grep({ search = query })
+      end),
+      mode = { "n", "x" },
+      desc = 'Grep operator',
+    },
+    {'<leader>fg', function() fzf.live_grep() end,            mode = 'n', desc = 'Live grep'},
+    {'<leader>fs', function() fzf.lgrep_curbuf() end,         mode = 'n', desc = 'Current buffer fuzzy find'},
+    -- Git --
+    {'<leader>gc', function() fzf.git_commits() end,          mode = 'n', desc = 'Find project git commits'},
+    {'<leader>gC', function() fzf.git_bcommits() end,         mode = 'n', desc = 'Find file git commits'},
+    {'<leader>gb', function() fzf.git_branches() end,         mode = 'n', desc = 'Find git branches'},
+    -- LSP/Diagnostics --
+    {'<leader>fd', function() fzf.diagnostics_document() end, mode = 'n', desc = 'List document diagnostics'},
+    -- Misc --
+    {'<leader>c',  function() fzf.colorschemes() end,         mode = 'n', desc = 'View colorschemes'},
+    {'<leader>fc', function() fzf.command_history() end,      mode = 'n', desc = 'View command history'},
+    {'<leader>fm', function() fzf.marks() end,                mode = 'n', desc = 'View marks'},
+    {'<leader>fr', function() fzf.registers() end,            mode = 'n', desc = 'View registers'},
+    {'<leader>k',  function() fzf.keymaps() end,              mode = 'n', desc = 'View keymaps'},
+  }
+end
 
-  {'<leader>fs', ':set operatorfunc=GrepOperator<cr>g@',                   mode = 'n', desc = 'Grep operator'},
-  {'<leader>fs', ':<c-u>call GrepOperator(visualmode())<cr>',              mode = 'v', desc = 'Grep operator'},
-}
-
-vim.api.nvim_exec([[
-  function! GrepOperator(type)
-    if a:type ==# 'v'
-      normal! `<v`>y
-    elseif a:type ==# 'char'
-      normal! `[v`]y
-    else
-      return
-    endif
-
-    silent execute "lua require('fzf-lua').grep({search='" . @@ . "'})"
-  endfunction
-]], false)
+    -- {'<leader>fs', ':set operatorfunc=GrepOperator<cr>g@',                   mode = 'n', desc = 'Grep operator'},
+    -- {'<leader>fs', ':<c-u>call GrepOperator(visualmode())<cr>',              mode = 'v', desc = 'Grep operator'},
+-- vim.api.nvim_exec([[
+--   function! GrepOperator(type)
+--     if a:type ==# 'v'
+--       normal! `<v`>y
+--     elseif a:type ==# 'char'
+--       normal! `[v`]y
+--     else
+--       return
+--     endif
+--
+--     silent execute "lua require('fzf-lua').grep({search='" . @@ . "'})"
+--   endfunction
+-- ]], false)
 
 Plugin.config = true
 
