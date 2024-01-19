116e5d95ae4b2fe1e0d36b0af0e8d82ee8293d41
Ethan Warrick <ewboy12@gmail.com>
2024-01-19 13:31:51 -0600

    Lang: Add javascript LSP
    This is also a typescript LSP

---
+++ lua/plugins/lang/js.lua
@@ -0,0 +1,15 @@
+return {
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+    optional = true,
+		opts = {
+			servers = {
+				-- Ensure mason installs the server
+        tsserver = {}, -- Requires Node.js | Might need to update Node.js
+			},
+		},
+	},
+
+}
