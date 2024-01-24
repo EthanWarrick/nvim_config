80a1428c000215beae3086bf41a78c516fa28a87
Ethan Warrick <ewboy12@gmail.com>
2024-01-24 10:39:36 -0600

    Plugin: tint: ignore diff windows
    This adds windows that are being diffed to the default list of ignored
    windows. The default function copied from the github page and the `diff`
    variable was added.

--- lua/plugins/tint.lua
+++ lua/plugins/tint.lua
@@ -2,6 +2,15 @@ local Plugin = { "levouh/tint.nvim" }
 
 Plugin.opts = {
   tint = -70,
+  window_ignore_function = function(winid)
+    local bufid = vim.api.nvim_win_get_buf(winid)
+    local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+    local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+    local diff = vim.api.nvim_win_get_option(winid, "diff")
+
+    -- Do not tint `terminal` or floating windows, tint everything else
+    return buftype == "terminal" or floating or diff
+  end
 }
 
 return Plugin
