858accdfacd0c2ab9482ab5e110afc0de29fca16
Ethan Warrick <ewboy12@gmail.com>
2024-01-25 09:43:27 -0600

    Keymap: esc out of floating windows

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -50,3 +50,13 @@ vim.keymap.set("n", "<leader>d", function()
     vim.diagnostic.disable()
   end
 end)
+
+-- Use escape to close all floating windows
+vim.keymap.set("n", "<esc>", function()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+    if config.relative ~= "" then -- is_floating_window?
+      vim.api.nvim_win_close(win, false) -- do not force
+    end
+  end
+end)
