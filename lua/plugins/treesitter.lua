095a9cdcfc1ed34c7d46cb22dc07c342daff3718
Ethan Warrick <ewboy12@gmail.com>
2024-01-26 17:58:38 -0600

    Plugin: treesitter: decrease load time
    Adding the init function here decreasing treesitter's startup time from
    ~500ms to ~15ms! I also decided to lazy load it and only update the
    parsers at build time.
    
    Credit for the init function goes to LazyVim:
    https://www.lazyvim.org/plugins/treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,21 @@ Plugin.dependencies = {
   { "nvim-treesitter/nvim-treesitter-textobjects" },
 }
 
+Plugin.event = "VeryLazy"
+Plugin.cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" }
+
+Plugin.build = ":TSUpdate"
+
+Plugin.init = function(plugin)
+  -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+  -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+  -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+  -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+  -- during startup.
+  require("lazy.core.loader").add_to_rtp(plugin)
+  require("nvim-treesitter.query_predicates")
+end
+
 -- See :help nvim-treesitter-modules
 Plugin.opts = {
   highlight = {
@@ -31,7 +46,6 @@ require("util").ts_ensure_installed({
 
 function Plugin.config(_, opts)
   require("nvim-treesitter.configs").setup(opts)
-  vim.cmd("silent TSUpdate")
 end
 
 return Plugin
