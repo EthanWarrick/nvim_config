f609edeaf97a80f5b4c41cd36f3230d03f196027
Ethan Warrick <ewboy12@gmail.com>
2024-01-31 11:29:02 -0600

    Plugin: fzf: Add file actions
    When I added this files action initially, I accidentally overwrote the
    already existing actions. This commit adds them back.

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -136,6 +136,11 @@ Plugin.config = function()
     actions = {
       files = {
         ["default"] = actions.file_edit,
+        ["ctrl-s"] = actions.file_split,
+        ["ctrl-v"] = actions.file_vsplit,
+        ["ctrl-t"] = actions.file_tabedit,
+        ["alt-q"] = actions.file_sel_to_qf,
+        ["alt-l"] = actions.file_sel_to_ll,
       },
     },
   })
