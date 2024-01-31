e5dc73f38e85dd03a0016aaeffba09bd70856f53
Ethan Warrick <ewboy12@gmail.com>
2024-01-31 11:23:57 -0600

    Plugin: comment: lazy-load

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ local Plugins = {
   { "tpope/vim-fugitive", cmd = "Git" },
   { "wellle/targets.vim" },
   { "tpope/vim-repeat" },
-  { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
+  { "numToStr/Comment.nvim", config = true, keys = { { "gc", mode = { "n", "x" } }, { "gb", mode = { "n", "x" } } } },
   { "christoomey/vim-tmux-navigator", keys = { "<c-h>", "<c-j>", "<c-k>", "<c-l>" } },
 
   { import = ... .. ".lang" },
