1658485772d48b4edb84bf2a0bce2db3d67e9f8b
Ethan Warrick <ewboy12@gmail.com>
2024-01-31 11:17:28 -0600

    Plugin: conform: disable lsp_fallback
    Conform won't auto format on save using the LSP's formatter. It will
    only format if there is a formatter specified for the filetype.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,19 +1,12 @@
 local Plugin = { "stevearc/conform.nvim" }
 
--- Plugin.opts = {
---   format_on_save = {
---     -- These options will be passed to conform.format()
---     timeout_ms = 500,
---     lsp_fallback = true,
---   },
--- }
 Plugin.opts = {
   format_on_save = function(bufnr)
     -- Disable with a global or buffer-local variable
     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
-    return { timeout_ms = 500, lsp_fallback = true }
+    return { timeout_ms = 500, lsp_fallback = false }
   end,
 }
 
