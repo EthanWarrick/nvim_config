9393c0c35a352ec37a35661d1a7cf9f6cdbe0dc8
Ethan Warrick <ewboy12@gmail.com>
2024-03-08 20:55:46 -0600

    Plugin: overseer: add
    Add the overseer plugin which allows for common commands to be run from
    inside neovim and provides an interface for viewing and interacting with
    them. Many build commands like make are automatically generated and
    supported. Custom commands can be specified in overseer's config or per
    project in `.vscode/tasks.json`.

---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,21 @@
+local Plugin = { "stevearc/overseer.nvim" }
+
+Plugin.cmd = {
+  "OverseerBuild",
+  "OverseerClearCache",
+  "OverseerClose",
+  "OverseerDeleteBundle",
+  "OverseerInfo",
+  "OverseerLoadBundle",
+  "OverseerOpen",
+  "OverseerQuickAction",
+  "OverseerRun",
+  "OverseerRunCmd",
+  "OverseerSaveBundle",
+  "OverseerTaskAction",
+  "OverseerToggle",
+}
+
+Plugin.config = true
+
+return Plugin
