e1ec417d8ef7d6c9f5bd55cc5de9850dec8a12e8
Ethan Warrick <ewboy12@gmail.com>
2024-03-14 10:40:49 -0500

    Lang: lua: reformat
    This is part of a general reformatting of lang files to better fit the
    existing format of other plugin files. There are no functional changes.

--- lua/plugins/lang/lua.lua
+++ lua/plugins/lang/lua.lua
@@ -1,29 +1,26 @@
-return {
+local Treesitter = require("util").ts_ensure_installed({ "lua" })
 
-  -- Treesitter
-  require("util").ts_ensure_installed({ "lua" }),
-
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    optional = true,
-    dependencies = { "folke/neodev.nvim", config = true },
-    opts = {
-      servers = {
-        -- Ensure mason installs the server
-        lua_ls = {},
-      },
-    },
-  },
-
-  -- Formatter
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-      },
+local LSP = {
+  "neovim/nvim-lspconfig",
+  optional = true,
+  dependencies = { "folke/neodev.nvim", config = true },
+  opts = {
+    servers = {
+      -- Ensure mason installs the server
+      lua_ls = {},
     },
   },
 }
+
+-- Formatter
+local Formatter = {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+  },
+}
+
+return { Treesitter, LSP, Formatter }
