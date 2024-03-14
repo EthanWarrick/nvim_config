97434c25dfcfcca93663ff14ea783aca1b0682a4
Ethan Warrick <ewboy12@gmail.com>
2024-03-14 10:39:15 -0500

    Lang: js: reformat
    This is part of a general reformatting of lang files to better fit the
    existing format of other plugin files. There are no functional changes.

--- lua/plugins/lang/js.lua
+++ lua/plugins/lang/js.lua
@@ -1,23 +1,20 @@
-return {
+local Treesitter = require("util").ts_ensure_installed({
+  "css",
+  "javascript",
+  "json",
+  "tsx",
+  "typescript",
+})
 
-  -- Treesitter
-  require("util").ts_ensure_installed({
-    "css",
-    "javascript",
-    "json",
-    "tsx",
-    "typescript",
-  }),
-
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    optional = true,
-    opts = {
-      servers = {
-        -- Ensure mason installs the server
-        tsserver = {}, -- Requires Node.js | Might need to update Node.js
-      },
+local LSP = {
+  "neovim/nvim-lspconfig",
+  optional = true,
+  opts = {
+    servers = {
+      -- Ensure mason installs the server
+      tsserver = {}, -- Requires Node.js | Might need to update Node.js
     },
   },
 }
+
+return { Treesitter, LSP }
