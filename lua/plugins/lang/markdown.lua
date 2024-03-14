ce6afc838ed95170bf6a93da034680cbecb4098e
Ethan Warrick <ewboy12@gmail.com>
2024-03-14 10:41:04 -0500

    Lang: markdown: reformat
    This is part of a general reformatting of lang files to better fit the
    existing format of other plugin files. There are no functional changes.

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,31 +1,26 @@
-return {
+local Treesitter = require("util").ts_ensure_installed({ "markdown" })
 
-  -- Treesitter
-  require("util").ts_ensure_installed({ "markdown" }),
-
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    optional = true,
-    opts = {
-      servers = {
-        -- Ensure mason installs the server
-        marksman = {},
-      },
+local LSP = {
+  "neovim/nvim-lspconfig",
+  optional = true,
+  opts = {
+    servers = {
+      -- Ensure mason installs the server
+      marksman = {},
     },
   },
-
-  -- A plugin to activate the python package grip to view markdown
-  --  files in a browser.
-  {
-    "PratikBhusal/vim-grip",
-    dependencies = { "joeyespo/grip", build = "pip3 install ." },
-    ft = { "markdown" }, -- Lazy load on these files
-    config = function()
-      vim.g.grip_default_map = 0 -- Don't use the default plugin keymappings
-
-      vim.keymap.set("n", "<leader>m", "<cmd>GripStart<cr>", { desc = "Start grip and open in browser" })
-      vim.keymap.set("n", "<leader>M", "<cmd>GripStop<cr>", { desc = "Kill grip" })
-    end,
-  },
 }
+
+local Extra = {
+  "PratikBhusal/vim-grip",
+  dependencies = { "joeyespo/grip", build = "pip3 install ." },
+  ft = { "markdown" }, -- Lazy load on these files
+  config = function()
+    vim.g.grip_default_map = 0 -- Don't use the default plugin keymappings
+
+    vim.keymap.set("n", "<leader>m", "<cmd>GripStart<cr>", { desc = "Start grip and open in browser" })
+    vim.keymap.set("n", "<leader>M", "<cmd>GripStop<cr>", { desc = "Kill grip" })
+  end,
+}
+
+return { Treesitter, LSP, Extra }
