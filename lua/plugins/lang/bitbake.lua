55f712b01405002207f425f388fa238f85fe1e0c
Ethan Warrick <ewboy12@gmail.com>
2024-03-14 10:38:45 -0500

    Lang: bitbake: reformat
    This is part of a general reformatting of lang files to better fit the
    existing format of other plugin files. There are no functional changes.

--- lua/plugins/lang/bitbake.lua
+++ lua/plugins/lang/bitbake.lua
@@ -1,16 +1,35 @@
-return {
+local Treesitter = require("util").ts_ensure_installed({ "bitbake" })
 
-  -- Treesitter
-  require("util").ts_ensure_installed({ "bitbake" }),
+-- local LSP = {
+--   "Freed-Wu/bitbake-language-server",
+--   -- build = "pip3.12 install .",
+--   build = "pip3.12 install bitbake-language-server",
+--   ft = "bitbake",
+--   -- init = function()
+--   --   -- Set conf files as bitbake filetypes
+--   --   -- pattern = { "*.bb", "*.bbappend", "*.bbclass", "*.inc", "conf/*.conf" },
+--   -- end,
+--   config = function()
+--     vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--       pattern = { "*.bb", "*.bbappend", "*.bbclass", "*.inc", "conf/*.conf" },
+--       callback = function()
+--         vim.lsp.start({
+--           name = "bitbake",
+--           cmd = { "bitbake-language-server" },
+--         })
+--       end,
+--     })
+--   end,
+-- }
 
-  -- Linter
-  {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        bitbake = { "oelint-adv" },
-      },
+local Linter = {
+  "mfussenegger/nvim-lint",
+  optional = true,
+  opts = {
+    linters_by_ft = {
+      bitbake = { "oelint-adv" },
     },
   },
 }
+
+return { Treesitter, Linter }
