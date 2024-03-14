b666cc23987b436644765b6539499c45080bf18e
Ethan Warrick <ewboy12@gmail.com>
2024-03-14 09:56:06 -0500

    Lang: python: reformat
    This is part of a general reformatting of lang files to better fit the
    existing format of other plugin files. There are no functional changes.

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -1,33 +1,29 @@
-return {
+local Treesitter = require("util").ts_ensure_installed({ "python", "rst" })
 
-  -- Treesitter
-  require("util").ts_ensure_installed({ "python", "rst" }),
-
-  -- LSP
-  {
-    "neovim/nvim-lspconfig",
-    optional = true,
-    opts = {
-      servers = {
-        -- Ensure mason installs the server
-        pylsp = {
-          handlers = {
-            ["textDocument/publishDiagnostics"] = function() end,
-          },
+local LSP = {
+  "neovim/nvim-lspconfig",
+  optional = true,
+  opts = {
+    servers = {
+      -- Ensure mason installs the server
+      pylsp = {
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function() end,
         },
-        ruff_lsp = {}, -- Additional ruff diagnostics
       },
-    },
-  },
-
-  -- Formatter
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        python = { "ruff" },
-      },
+      ruff_lsp = {}, -- Additional ruff diagnostics
     },
   },
 }
+
+local Formatter = {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      python = { "ruff" },
+    },
+  },
+}
+
+return { Treesitter, LSP, Formatter }
