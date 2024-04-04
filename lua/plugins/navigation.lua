ba18468dc1ef723bbac1f42634b0b33943921a0c
Ethan Warrick <ewboy12@gmail.com>
2024-04-04 12:59:20 -0500

    Plugin: zellij-nav: Add
    Zellij is another terminal multiplexer similar to Tmux. This provides
    keybinds to navigate between zellij panes and vim windows.

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,9 +1,9 @@
-local Plugin = { "christoomey/vim-tmux-navigator" }
+local Tmux = { "christoomey/vim-tmux-navigator" }
 
 -- Enbale Tmux navigator plugin only if nvim is opened from within Tmux
-Plugin.cond = os.getenv("TMUX") ~= nil
+Tmux.cond = os.getenv("TMUX") ~= nil
 
-Plugin.cmd = {
+Tmux.cmd = {
   "TmuxNavigateLeft",
   "TmuxNavigateDown",
   "TmuxNavigateUp",
@@ -11,7 +11,7 @@ Plugin.cmd = {
   "TmuxNavigatePrevious",
 }
 
-Plugin.keys = {
+Tmux.keys = {
   { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", { desc = "Tmux navigate left" } },
   { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", { desc = "Tmux navigate down" } },
   { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", { desc = "Tmux navigate up" } },
@@ -19,4 +19,25 @@ Plugin.keys = {
   { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", { desc = "Tmux navigate previous" } },
 }
 
-return Plugin
+local Zellij = { "swaits/zellij-nav.nvim" }
+
+-- Enbale Tmux navigator plugin only if nvim is opened from within Tmux
+Zellij.cond = os.getenv("ZELLIJ") ~= nil
+
+Zellij.config = true
+
+Zellij.cmd = {
+  "ZellijNavigateLeft",
+  "ZellijNavigateDown",
+  "ZellijNavigateUp",
+  "ZellijNavigateRight",
+}
+
+Zellij.keys = {
+  { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+  { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+  { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+  { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+}
+
+return { Tmux, Zellij }
