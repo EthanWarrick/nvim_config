1733e2ae28f22d156f2864165f3b29606066cfb9
Ethan Warrick <ewboy12@gmail.com>
2024-04-24 16:11:15 -0500

    Plugin: blame: Add
    This is an alternative to fugitive's `Git blame`. It feels a little more
    modern and has nice floating windows to provide blame information.

---
+++ lua/plugins/blame.lua
@@ -0,0 +1,19 @@
+local Plugin = { "FabijanZulj/blame.nvim" }
+
+Plugin.cmd = { "BlameToggle" }
+
+Plugin.keys = {
+  { "<leader>G", "<cmd>BlameToggle<cr>", mode = "n", desc = "Open git blame window" },
+}
+
+Plugin.opts = {
+  mappings = {
+    commit_info = "<leader><space>",
+    stack_push = "<TAB>",
+    stack_pop = "<BS>",
+    show_commit = "<CR>",
+    close = { "<esc>" },
+  },
+}
+
+return Plugin
