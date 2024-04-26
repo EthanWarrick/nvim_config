71ee2601a0283f5aa5014a4e9b665c8e6382be1b
Ethan Warrick <ewboy12@gmail.com>
2024-04-26 17:09:40 -0500

    init: add nvim bin folder
    This adds a bin foler to the ~/.local/share/nvim directory to hold
    programs and packages installed manually by the vim config. The Mason
    plugin already does something similar. This is how all of the LSPs and
    Linters are installed. I specifically want to install this to have a
    place to install fzf in order to have nvim instance of the program that
    doesn't interfere with the external version.

--- init.lua
+++ init.lua
@@ -3,6 +3,14 @@ local load = function(mod)
   require(mod)
 end
 
+-- Add a nvim specific bin directory for packages
+-- installed in this config.
+local bin_dir = vim.fn.stdpath("data") .. "/bin"
+vim.fn.mkdir(bin_dir, "p")
+if not string.find(vim.env.PATH, bin_dir) then
+  vim.env.PATH = bin_dir .. ":" .. vim.env.PATH
+end
+
 load("user.settings")
 load("user.commands")
 load("user.keymaps")
