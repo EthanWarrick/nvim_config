5f81e078809d4928d82c77e5332292113eda1ea2
Ethan Warrick <ewboy12@gmail.com>
2024-05-23 17:21:18 -0500

    Plugin: quickfix-reflector: add
    This plugin applies changes made to the entries of the quickfix list to
    the corresponding locations.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 local Plugins = {
   { "wellle/targets.vim", event = "VeryLazy" },
   { "numToStr/Comment.nvim", config = true, keys = { { "gc", mode = { "n", "x" } }, { "gb", mode = { "n", "x" } } } },
+  { "stefandtw/quickfix-reflector.vim", ft = "qf" },
 }
 
 return Plugins
