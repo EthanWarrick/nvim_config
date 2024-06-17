1888c868c277e512fa2d54c58a7323cd0df4046a
Ethan Warrick <ewboy12@gmail.com>
2024-06-17 14:45:35 -0500

    Keymap: close floating windows
    Use built-in functionality to close floating windows.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,18 +1,10 @@
 -- Space as leader key
 vim.g.mapleader = " "
 
-local close_floats = function()
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local config = vim.api.nvim_win_get_config(win)
-    if config.relative ~= "" then -- is_floating_window?
-      vim.api.nvim_win_close(win, false) -- do not force
-    end
-  end
-end
 vim.keymap.set("n", "<esc>", function()
-  close_floats()
+  vim.api.nvim_command("fclose")
   vim.api.nvim_command("nohlsearch")
-end, { desc = "Close all floating windows & disable search highlight" })
+end, { desc = "Close top floating window & disable search highlight" })
 
 -- Essentially creates a line text object
 --  - this is for operating on lines without counting the newline
