c09c1ffa257d2e04f909ea4666542277a7eaec96
Ethan Warrick <ewboy12@gmail.com>
2024-06-18 09:58:49 -0500

    Plugin: oil: Add netrw commands
    Adds the equivalent of most netrw commands using Oil. These open Oil in
    different splits.
    
    Also removed the <esc> keymap. I only want this for Oil floating windows
    which can be acheived with :fclose. This is already mapped to escape.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -18,7 +18,7 @@ Plugin.keys = {
       require("oil").open_float()
     end,
     mode = "n",
-    desc = "Open oil file explorer",
+    desc = "Open Oil file explorer as floating window",
   },
 }
 
@@ -34,12 +34,67 @@ Plugin.opts = {
     colorcolumn = "",
     signcolumn = "yes",
   },
-  keymaps = {
-    ["<Esc>"] = { callback = "actions.close", mode = "n", desc = "Close oil window" },
-  },
   view_options = {
     show_hidden = true,
   },
 }
+
+Plugin.config = function(_, opts)
+  local oil = require("oil")
+  oil.setup(opts)
+
+  vim.api.nvim_create_user_command("Explore", function()
+    oil.open()
+  end, { desc = "Explore directory of current file" })
+  vim.api.nvim_create_user_command("E", "Explore", { desc = "Shortcut for :Explore" })
+
+  vim.api.nvim_create_user_command("Hexplore", function(args)
+    if args.bang then
+      vim.api.nvim_command("aboveleft Oil")
+    else
+      vim.api.nvim_command("belowright Oil")
+    end
+  end, { bang = true, desc = "Horizontal Split & Explore" })
+  vim.api.nvim_create_user_command("He", function(args)
+    if args.bang then
+      vim.api.nvim_command("Hexplore!")
+    else
+      vim.api.nvim_command("Hexplore")
+    end
+  end, { bang = true, desc = "Shortcut for :Hexplore" })
+
+  vim.api.nvim_create_user_command("Vexplore", function(args)
+    if args.bang then
+      vim.api.nvim_command("rightbelow vertical Oil")
+    else
+      vim.api.nvim_command("leftabove vertical Oil")
+    end
+  end, { bang = true, desc = "Vertical Split & Explore" })
+  vim.api.nvim_create_user_command("Ve", function(args)
+    if args.bang then
+      vim.api.nvim_command("Vexplore!")
+    else
+      vim.api.nvim_command("Vexplore")
+    end
+  end, { bang = true, desc = "Shortcut for :Vexplore" })
+
+  vim.api.nvim_create_user_command("Sexplore", function(args)
+    if args.bang then
+      vim.api.nvim_command("vsplit +Oil")
+    else
+      vim.api.nvim_command("split +Oil")
+    end
+  end, { bang = true, desc = "Split & Explore current file's directory" })
+  vim.api.nvim_create_user_command("Se", function(args)
+    if args.bang then
+      vim.api.nvim_command("Sexplore!")
+    else
+      vim.api.nvim_command("Sexplore")
+    end
+  end, { bang = true, desc = "Shortcut for :Sexplore" })
+
+  vim.api.nvim_create_user_command("Texplore", "tabnew Oil", { desc = "Tab & Explore" })
+  vim.api.nvim_create_user_command("Te", "Texplore", { desc = "Shortcut for :Texplore" })
+end
 
 return Plugin
