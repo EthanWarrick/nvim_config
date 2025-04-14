aad1c85e91eff3e0a28e99ef2d3c4c3fba3833e4
Ethan Warrick <ewboy12@gmail.com>
2025-04-14 12:03:08 -0500

    Plugin: local-highligh: Disable animation
    The new update for local-highligh enables animation by default which
    depends on snacks.nvim. Disable animation to prevent an error message on
    startup.

--- lua/plugins/local-highlight.lua
+++ lua/plugins/local-highlight.lua
@@ -11,6 +11,18 @@ Plugin.opts = {
   cw_hlgroup = nil,
   -- Whether to display highlights in INSERT mode or not
   insert_mode = false,
+  min_match_len = 1,
+  max_match_len = math.huge,
+  highlight_single_match = true,
+  animate = {
+    enabled = false,
+    easing = "linear",
+    duration = {
+      step = 10, -- ms per step
+      total = 100, -- maximum duration
+    },
+  },
+  debounce_timeout = 200,
 }
 
 function Plugin.config(_, opts)
