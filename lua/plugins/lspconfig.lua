b9cea2b97c40e2ec1f268438ac4734a5c4b2bb45
Ethan Warrick <ewboy12@gmail.com>
2025-06-13 17:27:20 -0500

    Autocommand: Move LSP Keymap
    Keymaps are performed on the the LSPAttach event. Move these out of
    Lspconfig.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,5 @@
 ---@type LazyPluginSpec
 local Plugin = { "neovim/nvim-lspconfig" }
-local user = {}
 
 Plugin.dependencies = {
   { "williamboman/mason-lspconfig.nvim" },
@@ -46,14 +45,6 @@ Plugin.opts = {
 }
 
 function Plugin.config(_, opts)
-  -- Define buffer specific keymaps on LspAttach event
-  local group = vim.api.nvim_create_augroup("lsp_cmds", { clear = true })
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = group,
-    desc = "LSP actions",
-    callback = user.on_attach,
-  })
-
   local servers = opts.servers
   local has_cmp, cmp_nvim_lsp = pcall(require, "blink.cmp")
   local capabilities = vim.tbl_deep_extend(
@@ -104,28 +95,6 @@ function Plugin.config(_, opts)
   if have_mason then
     mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
   end
-end
-
--- Define keymapings
-function user.on_attach(args)
-  local bufmap = function(mode, lhs, rhs, desc)
-    local opts = { buffer = true, desc = desc }
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-
-  -- You can search each function in the help page.
-  -- For example :help vim.lsp.buf.hover()
-  bufmap("n", "gd", vim.lsp.buf.definition, "Go to definition")
-  bufmap("n", "gD", vim.lsp.buf.declaration, "Go to declartation")
-  bufmap("n", "go", vim.lsp.buf.type_definition, "Go to type definition")
-  bufmap("n", "gl", vim.diagnostic.open_float, "Open diagnostic float")
-  -- bufmap('n', 'gs', vim.lsp.buf.signature_help, "")
-  bufmap({ "n", "x" }, "<F3>", function()
-    vim.lsp.buf.format({ async = true })
-  end, "")
-  bufmap("n", "<F4>", vim.lsp.buf.code_action, "")
-
-  vim.bo[args.buf].tagfunc = "" -- Stop Neovim from setting the LSP as the tag provider
 end
 
 return Plugin
