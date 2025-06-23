1f910362367f28660e17acbac8ea0c1ca0a58ae7
Ethan Warrick <ewboy12@gmail.com>
2025-06-23 09:21:53 -0500

    Plugin: lazy: stop auto reload config
    This stops lazy from trying to auto reload the config for existing
    neovim processes. This happens if any change is made that affects the
    file hash(?) of a file within the nvim config dir. Auto reloading the
    config tends to break the existing sessions without actually bringing in
    the new config. Also, the processes that tried to reload the config
    become hard to kill.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,4 +42,9 @@ require("lazy").setup({
       },
     },
   },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = false,
+    notify = false, -- get a notification when changes are found
+  },
 })
