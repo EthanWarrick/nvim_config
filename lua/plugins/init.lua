b82a844ac4064e39ccd975fea11458f2aa18f05e
Ethan Warrick <ewboy12@gmail.com>
2026-05-28 16:06:10 -0500

    Plugin: demicolon: Add
    Add the demicolon plugin to make all jumps using `[` or `]` repeatable
    with the `;` and `,` keys. This mimics the repeatable nature of the `f`
    and `t` commands. Press semicolon to repeat the previous, multi-key,
    clunky jump command.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,19 @@ local Plugins = {
       { "gbO", function() require("Comment.api").insert.blockwise.above({ padding = true }) end, desc = "Block comment above line" },
     },
   },
+  {
+    ---@module 'demicolon'
+    "mawkler/demicolon.nvim",
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    lazy = false,
+    ---@type demicolon.options
+    opts = {
+      keymaps = { repeat_motions = 'stateless' } -- I want to do statefull but it doesn't work...
+    },
+  }
 }
 
 return Plugins
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -253,26 +253,6 @@ local Treesitter_Text_Objects = {
             })
           end
         end
-
-
-
-        local ts_repeat_move = require "nvim-treesitter-textobjects.repeatable_move"
-
-        -- Repeat movement with ; and ,
-        -- ensure ; goes forward and , goes backward regardless of the last direction
-        -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next, { buf = buf })
-        -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous, { buf = buf })
-
-        -- vim way: ; goes to the direction you were moving.
-        vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move, { buf = buf })
-        vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite, { buf = buf })
-
-        -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-        vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true, buf = buf })
-        vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true, buf = buf })
-        vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true, buf = buf })
-        vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true, buf = buf })
-
       end
 
       if vim.tbl_get(opts, "select", "enable") and have(ft, "textobjects") then
