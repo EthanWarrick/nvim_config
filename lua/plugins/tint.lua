76af9061c2dfe3212ddfd9e53a5e9538626a6eeb
Ethan Warrick <ewboy12@gmail.com>
2026-05-29 14:23:30 -0500

    Plugin: tint: Remove deprications
    Remove the depricated nvim api calls.

--- lua/plugins/tint.lua
+++ lua/plugins/tint.lua
@@ -10,9 +10,9 @@ Plugin.opts = {
   tint = -55, -- Determines the intensity of dimming
   window_ignore_function = function(winid)
     local bufid = vim.api.nvim_win_get_buf(winid)
-    local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = bufid })
     local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
-    local diff = vim.api.nvim_win_get_option(winid, "diff")
+    local diff = vim.api.nvim_get_option_value("diff", { win = winid })
 
     -- Do not tint `terminal`, floating windows or diff tint everything else
     return buftype == "terminal" or floating or diff
